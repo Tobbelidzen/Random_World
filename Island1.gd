4bcce3b5277a2ae90bf7d113ac13434731fd4ecb
Tobias Lidzen <tobias.lidzen@gmail.com>
2020-03-01 19:19:37 +0100

    La till rörelse i bakgrunden och randomiserar sprites

--- Island1.gd
+++ Island1.gd
@@ -4,12 +4,23 @@ extends Sprite
 # var a = 2
 # var b = "text"
 var fart
+var sprite
 onready var plane = get_node("../../Physicsbody/Plane")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(rand_range(1200,1300),rand_range(0,800))
+	randomize()
+	position = Vector2(rand_range(1200,1300),rand_range(100,600))
+	sprite = randi() % 3
+	print(sprite)
+	
+	if sprite == 0:
+		self.region_rect = Rect2(Vector2(2,2), Vector2(62,63))
+	elif sprite == 1:
+		self.region_rect = Rect2(Vector2(67,2), Vector2(62,63))
+	else:
+		self.region_rect = Rect2(Vector2(132,2), Vector2(62,63))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position -= Vector2(1,0)
+	position -= Vector2(2,0)
